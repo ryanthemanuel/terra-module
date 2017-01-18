8e6477a21e1d41eed8446037ac9635b7b5a362be
Ryan Manuel <rfmanuel@gmail.com>
2017-01-18 16:27:56 -0600

    Update to use relative path for the imported sass file.

--- src/TerraModule.jsx
+++ src/TerraModule.jsx
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 import path from 'path';
-import './terra-module.scss';
+import '../src/terra-module.scss';
 
 class TerraModule extends React.Component {
   constructor() {
